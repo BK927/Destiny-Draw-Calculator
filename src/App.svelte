89ef3bb8d4eb82f193e0e41858668a3717036e41
BK927 <dead4bees927@gmail.com>
2022-03-09 15:49:58 +0900

    update1

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,67 @@
-<script>
-	export let name;
+<script lang="ts">
+    <link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />;
+    <link rel="stylesheet" href="/smui.css" media="(prefers-color-scheme: light)" />
+<link
+  rel="stylesheet"
+  href="/smui-dark.css"
+  media="screen and (prefers-color-scheme: dark)"
+/>
+    import Textfield from "@smui/textfield";
+    import HelperText from "@smui/textfield/helper-text";
+    let deckInput = "";
+    let deckNum = "";
+
+    function inputDeckNum(event) {
+        if (isNaN(deckInput)) {
+            deckInput = deckNum;
+        } else {
+            deckNum = deckInput;
+        }
+        console.log("called");
+    }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <div>
+        <div>
+            <Textfield on:input={inputDeckNum} variant="outlined" bind:value={deckInput} label="Outlined">
+                <HelperText persistent slot="helper">Helper Text</HelperText>
+            </Textfield>
+
+            <pre class="status">Value: {deckNum}</pre>
+        </div>
+    </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    * :global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__leading) {
+        border-radius: 28px 0 0 28px;
+        width: 28px;
+    }
+    * :global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__trailing) {
+        border-radius: 0 28px 28px 0;
+    }
+    * :global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__notch) {
+        max-width: calc(100% - 28px * 2);
+    }
+    * :global(.shaped-outlined.mdc-text-field--with-leading-icon:not(.mdc-text-field--label-floating) .mdc-floating-label) {
+        left: 16px;
+    }
+    * :global(.shaped-outlined + .mdc-text-field-helper-line) {
+        padding-left: 32px;
+        padding-right: 28px;
+    }
 </style>
